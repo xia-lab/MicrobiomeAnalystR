27cab6c19bb3636a68585ef2aec3cbdcd0de2127
jsychong <jasmine.chong@mail.mcgill.ca>
2019-05-28 10:23:14 -0400

    delete duplicate code

--- setParameters.R
+++
@@ -1,7 +0,0 @@
-setParameters <- function(file_compressed = TRUE, # logic TURE or FALSE, compressed format gz, bz2,
-                          OS_is_windows = FALSE, # operation system is windows, linux or mac;
-                          ... ){
-  parameters <- list(file_compressed = file_compressed,
-                     OS_is_windows = OS_is_windows);
-  return(parameters);
-}
